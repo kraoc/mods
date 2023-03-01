20cacdc17271a5ed3070f2a5af1e85a4a507324f
Olivier <kraoc@users.noreply.github.com>
2023-03-01 18:45:52 +0100

    Update server.sh

--- tools/server.sh
+++ tools/server.sh
@@ -35,7 +35,7 @@ function UpdateFerium() {
     mkdir -p tools >/dev/null 2>&1
     cd tools/
         rm -f ferium >/dev/null 2>&1
-        wget --header="Cache-Control: no-cache, no-store, must-revalidate" --header="Pragma: no-cache" --header="Expires: 0" --output-document=ferium --no-clobber --no-dns-cache --inet4-only --no-cache --no-cookies --no-check-certificate --recursive $FeriumApp >/dev/null 2>&1
+        wget --header="Cache-Control: no-cache, no-store, max-age=0, must-revalidate" --header="Pragma: no-cache" --header="Expires: 0" --output-document=ferium --no-clobber --no-dns-cache --inet4-only --no-cache --no-cookies --no-check-certificate --recursive $FeriumApp >/dev/null 2>&1
     cd ..
 }
 
@@ -47,9 +47,9 @@ function UpdateDatas() {
         rm -f version.txt >/dev/null 2>&1
         rm -f modrinth.txt >/dev/null 2>&1
         rm -f curseforge.txt >/dev/null 2>&1
-        wget --header="Cache-Control: no-cache, no-store, must-revalidate" --header="Pragma: no-cache" --header="Expires: 0" --output-document=version.txt --no-clobber --no-dns-cache --inet4-only --no-cache --no-cookies --no-check-certificate --recursive $ListVersion >/dev/null 2>&1
-        wget --header="Cache-Control: no-cache, no-store, must-revalidate" --header="Pragma: no-cache" --header="Expires: 0" --output-document=modrinth.txt --no-clobber --no-dns-cache --inet4-only --no-cache --no-cookies --no-check-certificate --recursive $ListModrinth >/dev/null 2>&1
-        wget --header="Cache-Control: no-cache, no-store, must-revalidate" --header="Pragma: no-cache" --header="Expires: 0" --output-document=curseforge.txt --no-clobber --no-dns-cache --inet4-only --no-cache --no-cookies --no-check-certificate --recursive $ListCurseforge >/dev/null 2>&1
+        wget --header="Cache-Control: no-cache, no-store, max-age=0, must-revalidate" --header="Pragma: no-cache" --header="Expires: -1" --output-document=version.txt --no-clobber --no-dns-cache --inet4-only --no-cache --no-cookies --no-check-certificate --recursive $ListVersion >/dev/null 2>&1
+        wget --header="Cache-Control: no-cache, no-store, max-age=0, must-revalidate" --header="Pragma: no-cache" --header="Expires: -1" --output-document=modrinth.txt --no-clobber --no-dns-cache --inet4-only --no-cache --no-cookies --no-check-certificate --recursive $ListModrinth >/dev/null 2>&1
+        wget --header="Cache-Control: no-cache, no-store, max-age=0, must-revalidate" --header="Pragma: no-cache" --header="Expires: -1" --output-document=curseforge.txt --no-clobber --no-dns-cache --inet4-only --no-cache --no-cookies --no-check-certificate --recursive $ListCurseforge >/dev/null 2>&1
     cd ..
 }
 
